8e1d62a0b8ddd2a38d318390fda98b9eb415b51e
Mudassar Hassan <mudassarhassan135@gmail.com>
2025-02-15 13:15:25 +0000

    Chore: Fix lint

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,13 +2,13 @@ class FilmsController < ApplicationController
   def show
     film = SwapiService.get_film params[:id]
 
-    film['characters'] = fetch_film_characters(film['characters'])
-    
+    film["characters"] = fetch_film_characters(film["characters"])
+
     render inertia: "films/show", props: { film: }
   end
-  
+
   private
-  
+
   def fetch_film_characters(character_uris)
     character_uris.map do |character_uri|
       character_id = extract_character_id(character_uri)
@@ -18,7 +18,7 @@ class FilmsController < ApplicationController
       { label: character["name"], href: person_path(character_id) }
     end
   end
-  
+
   def extract_character_id(character_uri)
     URI(character_uri).path.split("/").last
   end
